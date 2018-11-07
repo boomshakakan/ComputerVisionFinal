0c43f8fd44fa0df8a5c6ef1185364c3f54bd660e
boomshakakan <msteve20@uic.edu>
2018-11-07 15:32:08 -0600

    POSSIBLY WORKING CMAKE CONFIGURATION!!!

---
+++ CMakeFiles/imgProcess.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/imgProcess.dir/src/imgProcess.cpp.o"
+  "imgProcess.pdb"
+  "imgProcess"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/imgProcess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
